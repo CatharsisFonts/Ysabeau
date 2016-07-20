130061511cc1bbd90e02882519b66807ad812bc7
Christian Thalmann <christian.d.thalmann@gmail.com>
2016-07-20 22:39:45 +0200

    Tweaked Bulgarian, implemented ss09, ss10, added @u kerning class

--- 3. Glyphs Source Files/Eau Make Export Instances.py
+++ 3. Glyphs Source Files/Eau Make Export Instances.py
@@ -23,12 +23,19 @@ for x in forbidden:
 	if x in all02names: 
 		good02names.remove(x)
 		
-renameGlyphs2 = [ "%s=%s" % ( x, x.replace(suffix,"") ) for x in all02names ]
-renameGlyphsParameterKey = "Rename Glyphs"
+renameGlyphs2 = [ "%s=%s" % ( x, x.replace(suffix,"") ) for x in good02names ]
+
+
+forbidden = all01names
+forbidden = [ x.replace(".ss01", ".loclBGR") for x in forbidden ]
+
+suffix = ".loclBGR"
+allBGRnames = [ g.name for g in Font.glyphs if (g.name.find(suffix) > 0) ]		
+renameGlyphs3 = [ "%s=%s" % ( x, x.replace(suffix,"") ) for x in allBGRnames ]
 
 suffix = ".lf"
 allLFGlyphNames = [ g.name for g in Font.glyphs if g.name.endswith(suffix) ]
-renameGlyphs3 = [ "%s=%s" % ( x, x.replace(suffix,"") ) for x in allLFGlyphNames ]
+renameGlyphs4 = [ "%s=%s" % ( x, x.replace(suffix,"") ) for x in allLFGlyphNames ]
 
 #decomposeGlyphs = all01names + allLFGlyphNames
 decomposeGlyphs = [g.name for g in Font.glyphs]
@@ -41,8 +48,8 @@ for thisInstance in Font.instances:
 		familyName = Font.familyName
 	if familyName.endswith("Infant"):
 		thisInstance.removeObjectFromCustomParametersForKey_( renameGlyphsParameterKey )
-		thisInstance.setCustomParameter_forKey_( renameGlyphs1 + renameGlyphs2 + renameGlyphs3, renameGlyphsParameterKey )
-		thisInstance.setCustomParameter_forKey_( ["ss01", "ss02", "lnum"], "Remove Features" )
+		thisInstance.setCustomParameter_forKey_( renameGlyphs1 + renameGlyphs2 + renameGlyphs3 + renameGlyphs4, renameGlyphsParameterKey )
+		thisInstance.setCustomParameter_forKey_( ["ss01", "ss02", "lnum", "locl"], "Remove Features" )
 		thisInstance.removeObjectFromCustomParametersForKey_( "Decompose Glyphs" )
 		thisInstance.setCustomParameter_forKey_( decomposeGlyphs, "Decompose Glyphs" )
 
